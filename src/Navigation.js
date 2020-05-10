90cb422a586671e33e9148f428dd21c6a3536d28
max <max.smith@duke.edu>
2020-05-09 22:44:06 -0400

    added link hover effects and target smooth scroll

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navigation.css';
-import { Link, animateScroll as scroll} from 'react-scroll';
+import { Link } from 'react-scroll';
 
 class Navigation extends React.Component {
 
