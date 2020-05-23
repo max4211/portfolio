97e0a126de2a1d6dbf4c3e1d1d98c23d4e0ea3d5
max <max.smith@duke.edu>
2020-05-23 19:44:50 -0400

    created custom links with animated underline

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,10 @@ class About extends React.Component {
         return(
             <div className="about-container" id="about">
                 <h1>About Me</h1>
-                <h2>Coming Soon...</h2>
+                <p>Hello! I'm Max, a software engineer based in Potomac, MD.</p>
+                <p>I enjoy creating things...</p>
+                <p>I am a rising senior at <a className="custom-link" href="https://duke.edu/">Duke University</a> studying Computer Science, Electrical and Computer Engineering, and Economics. I will be working at <a className="custom-link" href="https://www.capitalone.com/">Capital One</a> this summer as a Software Engineer.</p>
+                <p>Here are some technologies I've been working with:</p>
             </div>
         )
     }
