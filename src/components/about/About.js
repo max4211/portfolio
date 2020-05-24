c723f132afb60349b053b20003e8254bb3dfff21
max <max.smith@duke.edu>
2020-05-24 17:44:37 -0400

    updated about me section with picture and technologies

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './About.css';
+import cliffsImg from '../../img/cliffs.jpg';
+import Skills from './Skills';
+
 
 class About extends React.Component {
 
     render() {
         return(
             <div className="about-container" id="about">
-                <h1>About Me</h1>
-                <p>Hello! I'm Max, a software engineer based in Potomac, MD.</p>
-                <p>I enjoy creating things...</p>
-                <p>I am a rising senior at <a className="custom-link" href="https://duke.edu/">Duke University</a> studying Computer Science, Electrical and Computer Engineering, and Economics. I will be working at <a className="custom-link" href="https://www.capitalone.com/">Capital One</a> this summer as a Software Engineer.</p>
-                <p>Here are some technologies I've been working with:</p>
+                <div className="about-text">
+                    <h1>About Me</h1>
+                    <p>Hello! I'm Max, a software engineer based in Potomac, MD.</p>
+                    <p>I enjoy creating things...</p>
+                    <p>I am a rising senior at <a className="custom-link" href="https://duke.edu/">Duke University</a> studying Computer Science, Electrical and Computer Engineering, and Economics. I will be working at <a className="custom-link" href="https://www.capitalone.com/">Capital One</a> this summer as a Software Engineer.</p>
+                    <p>Here are some technologies I've been working with:</p>
+                    <Skills />
+                </div>
+                <div className="cliffs-photo">
+                    <img  src={cliffsImg} alt="Portrait of Max @ Cliffs of Mohrer"/>
+                </div>
             </div>
         )
     }
---
+++ src/components/about/Skills.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Skills.css';
+
+class Skills extends React.Component {
+
+    render() {
+        return(
+            <ul className="skills-ul">
+                <li>Java</li>
+                <li>Python</li>
+                <li>MATLAB</li>
+                <li>HTML & CSS</li>
+                <li>React</li>
+                <li>JavaScript</li>
+            </ul>
+        )
+    }
+}
+
+export default Skills;
