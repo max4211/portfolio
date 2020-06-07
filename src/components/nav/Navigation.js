cf4311c3f0537551dc40fe6b580d14f0daf15f32
max <max.smith@duke.edu>
2020-06-07 14:49:23 -0400

    created responsive nav for mobile, ul slides in from right

--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -14,34 +14,31 @@ class Navigation extends React.Component {
         let toggleNavStatus = false;
 
         function toggleHamburger() {
-            console.log("toggling hamburger function");
             $(".icon").toggleClass("close");
         }
 
+        function selectedItem() {
+            $(".icon").toggleClass("close");
+            let getSidebar = document.querySelector(".nav-main");
+            getSidebar.style.right = "-75vw";
+            toggleNavStatus = false;
+            // getSidebar.style.display = "none";
+
+        }
+
         function toggleNav() {
-            console.log('running toggleNav, clicked on button');
             /* Get all HTML elements we want to change styling of */
             let getSidebar = document.querySelector(".nav-main");
-            let getSidebarUl = document.querySelector(".nav-main ul");
-            let getSidebarLinks = document.querySelectorAll(".nav-main a");
         
             /* Check toggle nav status */
             if (toggleNavStatus === false) {
-                getSidebar.style.width = "272px";
-                getSidebarUl.style.visibility = "visible";
-                
-                for (let i = 0; i < getSidebarLinks.length; i ++) {
-                    getSidebarLinks[i].style.opacity = "1";
-                }
+                getSidebar.style.right = "0";
+                // getSidebar.style.display = "block";
         
                 toggleNavStatus = true;
             } else if (toggleNavStatus === true) {
-                getSidebar.style.width = "50px";
-                getSidebarUl.style.visibility = "hidden";
-                
-                for (let i = 0; i < getSidebarLinks.length; i ++) {
-                    getSidebarLinks[i].style.opacity = "0";
-                }
+                getSidebar.style.right = "-75vw";
+                // getSidebar.style.display = "none";
         
                 toggleNavStatus = false;
             }
@@ -65,6 +62,7 @@ class Navigation extends React.Component {
                                 spy={true}
                                 smooth={true}
                                 duration={500}
+                                onClick={selectedItem}
                                 >About</Link></li>
                             <li className="nav-item active"><Link
                                 to="experience"
