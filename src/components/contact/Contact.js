adbc2d3cb29996bda69913bd5ac45847c5a3b5ff
max <max.smith@duke.edu>
2020-09-24 20:02:13 -0400

    Updated to fontawesome 5, burger icon for portfolio

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Contact.css';
 import FontAwesome from 'react-fontawesome';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHamburger } from '@fortawesome/free-solid-svg-icons'
 
 class Contact extends React.Component {
 
@@ -10,6 +11,7 @@ class Contact extends React.Component {
             <div className="contact-container" id="contact">
                 <div className="social-icon-div" orientation="left">
                     <ul className="social-icons">
+                        <li><a href="http://www.teambigeats.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faHamburger} /></a></li>
                         <li><a href="https://www.facebook.com/maxaaronsmith" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-facebook"/></a></li>
                         <li><a href="https://www.instagram.com/maxsmith4211/" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-instagram"/></a></li>
                         <li><a href="https://www.linkedin.com/in/max-smith-40a65b14a/" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-linkedin"/></a></li>
