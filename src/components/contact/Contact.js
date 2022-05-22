6dcce5dbdd8a20b8ad3d93a1bdf70b3e8705980a
Max Smith <ms724@duke.edu>
2022-05-21 19:43:22 -0700

    Removes social icons

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -12,8 +12,8 @@ class Contact extends React.Component {
                 <div className="social-icon-div" orientation="left">
                     <ul className="social-icons">
                         <li><a href="http://www.teambigeats.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faHamburger} /></a></li>
-                        <li><a href="https://www.facebook.com/maxaaronsmith" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-facebook"/></a></li>
-                        <li><a href="https://www.instagram.com/maxsmith4211/" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-instagram"/></a></li>
+                        {/* <li><a href="https://www.facebook.com/maxaaronsmith" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-facebook"/></a></li> */}
+                        {/* <li><a href="https://www.instagram.com/maxsmith4211/" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-instagram"/></a></li> */}
                         <li><a href="https://www.linkedin.com/in/max-smith-40a65b14a/" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-linkedin"/></a></li>
                         <li><a href="https://github.com/max4211?tab=repositories" target="_blank" rel="noopener noreferrer"><FontAwesome className="fa fa-github"/></a></li>
                     </ul>
